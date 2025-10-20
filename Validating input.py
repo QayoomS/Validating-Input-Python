8fb04865abeea43de5873944b50fd1d9b4002e7a
Qayoom Suroush <133157900+QayoomS@users.noreply.github.com>
2025-10-20 17:07:39 +0100

    Refactor input validation function and add comments
    Refactored input validation logic and added comments for clarity.

--- Validating input.py
+++ Validating input.py
@@ -1,9 +1,12 @@
+# define a function that takes n and return true or false based on min and max allowed range
 def not_in_range(n):
     return True if min_n <= n <= max_n else False
 
-
+# defining and seting the n, max and min n for the program:
 min_n, max_n, input_n = 1, 10, -1
 
+
+#printing a welcome message:
 print('Welcome to checking input validation program')
 
 while not not_in_range(input_n):
@@ -18,4 +21,5 @@ while not not_in_range(input_n):
             print('Your integer is between ' + f'{min_n} and {max_n}')
         else:
             print('Your integer is not between ' + f'{min_n} and {max_n}')
+
 print('Program completed. you chose: ', input_n)
